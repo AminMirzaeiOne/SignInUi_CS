8e24c19ac0702f8f0f25caf6efd5865657540411
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-16 23:39:54 +0330

    - Update and Change AssmblyInfo Project

--- SignInUi/Properties/AssemblyInfo.cs
+++ SignInUi/Properties/AssemblyInfo.cs
@@ -8,12 +8,12 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SignInUi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WPF Modern login page design")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AminMirzaeiOne")]
 [assembly: AssemblyProduct("SignInUi")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("AminMirzaeiOne")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
